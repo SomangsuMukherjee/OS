486e596b86326291b4c8bbe5e2f6bb34d9be1361
Sarukesh Boominathan <sarukesh.boominatha@student.tuke.sk>
2024-12-16 20:33:06 +0100

    Complete

--- Project/fbSystem/admin.py
+++ Project/fbSystem/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Feedback
 
-# Register your models here.
+@admin.register(Feedback)
+class FeedbackAdmin(admin.ModelAdmin):
+    list_display = ('name', 'faculty', 'review', 'image') 
+    search_fields = ('name', 'faculty', 'review')          
+    list_filter = ('faculty',)                             
+    ordering = ('name',)                                   
